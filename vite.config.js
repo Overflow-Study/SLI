420b67ca2381ebec5dacad1ec0f59beae2707364
paulsea27115 <csb070115@gmail.com>
2024-06-22 22:07:57 +0900

    [feat] 배포성공

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    chunkSizeWarningLimit: 1600,
-    manifest: true,
-    rollupOptions: {
-      input: {
-        main: "./index.html",
-      },
-    },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
   },
   plugins: [react()],
 })

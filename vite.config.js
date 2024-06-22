d43b17ebcfd61e4961fb60244211949945439c9e
paulsea27115 <csb070115@gmail.com>
2024-06-22 21:49:32 +0900

    [feat] vite json 수정

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    port: 80,
+  build: {
+    chunkSizeWarningLimit: 1600,
+    manifest: true,
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+      },
+    },
   },
   plugins: [react()],
 })
